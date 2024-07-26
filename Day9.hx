df59a22d0778ab9a287db7a2266d7bc21fcf0ed1
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-07-26 15:40:12 -0600

    Update Day9.hx

--- Day9.hx
+++ Day9.hx
@@ -15,23 +15,19 @@ class Day9 {
 
     static function parsefile() {
         var con: AS = [for (i in sys.io.File.getContent('Advent Files_2021/Day9.txt').split('\n')) trim(i)];
-        var nines = [for (i in 0...con[0].length + 2) '9'].join("");
-        var arr: AS = [nines].concat([for (i in con) i.insertAt(0, '9') + '9']).concat([nines]);
-        var arr: AAI = [for (i in arr) i.split('').map(item -> parseInt(item))]; 
+        var arr: AAI = [for (i in con) i.split('').map(item -> parseInt(item))]; 
         return arr;
     }
 
     static function solution1(arr: AAI) {
         var ttl = 0;
-        var row_len = arr.length - 1;
-        var col_len = arr[0].length - 1;
         var lows: AAI = [];
-        for (i in 1...row_len) {
-            for (j in 1...col_len) {
-                if (arr[i][j] < arr[i][j-1] && arr[i][j] < arr[i][j+1] &&
-                    arr[i][j] < arr[i-1][j] && arr[i][j] < arr[i+1][j]) { 
-                        ttl += arr[i][j] + 1;
-                        lows.push([i, j]);
+        for (row => rval in arr) {
+            for (col => cval in rval) {
+                var tmp = nbrs(arr, [row, col]).vals;
+                if (tmp.filter(item -> item > cval).length == tmp.length) {
+                    ttl += cval + 1;
+                    lows.push([row, col]);
                 }
             }
         }
@@ -39,7 +35,6 @@ class Day9 {
     }
 
     static function solution2(arr: AAI, lows: AAI) {
-        var dir = [[0, 1], [0, -1], [1, 0], [-1, 0]];
         var basin = [for (i in lows) [i]];
         var m = [for (i in lows) [i.join("") => arrValue(arr, i)]];
         
@@ -48,14 +43,9 @@ class Day9 {
                 var tmp: AAI = [];
                 for (j in i) {
                     var lowest: Int = arrValue(arr, j);
-                    for (k in dir) {
-                        var adj_val: Int = arrValue(arr, addArrs(j, k));
-                        var adj_ind = addArrs(j, k);
-                        if (adj_val != 9 && adj_val > lowest && !m[ind].exists(adj_ind.join(""))) {
-                            tmp.push(adj_ind);
-                            m[ind][adj_ind.join("")] = adj_val;
-                        }
-                    } 
+                    var n4 = nbrs(arr, j).indices.filter(item -> arrValue(arr, item) != 9 && cast(arrValue(arr, item), Int) > lowest && !m[ind].exists(item.join("")));
+                    tmp = tmp.concat(n4);
+                    for (vals in n4) m[ind][vals.join("")] = arrValue(arr, vals);
                 }
                 basin[ind] = tmp;
             }
