f5e69dfb47a648ce8615420ea4df8b93febfed81
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-08-04 19:46:55 -0600

    Update Day9.hx

--- Day9.hx
+++ Day9.hx
@@ -9,14 +9,13 @@ class Day9 {
     static function main() {
         var arr = parsefile();
         var s1 = solution1(arr);
-        var s2 = solution2(arr, s1.lows);
+        var m = [for (i in s1.lows) [i.join("") => cast(arrValue(arr, i), Int)]];
+        var s2 = solution2(arr, [for (i in s1.lows) [i]], m);
         Sys.println('Part 1: ${s1.pt1}\nPart 2: ${s2}');
     }
 
-    static function parsefile() {
-        var con: AS =   [for (i in sys.io.File.getContent('Advent Files_2021/Day9.txt').split('\n')) trim(i)];
-        var arr: AANI = [for (i in con) i.split('').map(item -> parseInt(item))]; 
-        return arr;
+    static inline function parsefile() {
+        return [for (i in sys.io.File.getContent('Advent Files_2021/Refac.txt').split('\n')) trim(i).split('').map(item -> parseInt(item))]; 
     }
 
     static function solution1(arr: AANI) {
@@ -34,24 +33,23 @@ class Day9 {
         return {lows: lows, pt1: ttl};
     }
 
-    static function solution2(arr: AANI, lows: AAI) {
-        var basin = [for (i in lows) [i]];
-        var m = [for (i in lows) [i.join("") => arrValue(arr, i)]];
-        
-        while (basin.count(item -> item.empty()) != basin.length) {
-            for (ind => i in basin) {
-                var tmp: AAI = [];
-                for (j in i) {
-                    var lowest: Int = arrValue(arr, j);
-                    var n4 = nbrs(arr, j).indices.filter(item -> arrValue(arr, item) != 9 && cast(arrValue(arr, item), Int) > lowest && !m[ind].exists(item.join("")));
-                    tmp = tmp.concat(n4);
-                    for (vals in n4) m[ind][vals.join("")] = arrValue(arr, vals);
-                }
-                basin[ind] = tmp;
+    static function solution2(arr: AANI, lows: AAAI, m: Array<MSI>) {
+        if (lows.count(item -> item.empty()) == lows.length) {
+            var sol = [for (i in m) i.count()];
+            sol.sort((a, b) -> b - a);
+            return intProd(sol.slice(0, 3));
+        } 
+
+        for (ind => i in lows) {
+            var tmp: AAI = [];
+            for (j in i) {
+                var lowest: Int = arrValue(arr, j);
+                var n4 = nbrs(arr, j).indices.filter(item -> arrValue(arr, item) != 9 && cast(arrValue(arr, item), Int) > lowest && !m[ind].exists(item.join("")));
+                tmp = tmp.concat(n4);
+                for (vals in n4) m[ind][vals.join("")] = arrValue(arr, vals);
             }
+            lows[ind] = tmp;
         }
-        var sol = [for (i in m) i.count()];
-        sol.sort((a, b) -> b - a);
-        return intProd(sol.slice(0, 3));
+        return solution2(arr, lows, m);
     }   
 }
