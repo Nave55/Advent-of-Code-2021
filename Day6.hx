c85133f97b32079a5df02ab4a3433f8a92efd985
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-08-19 14:17:43 -0600

    Update Day6.hx

--- Day6.hx
+++ Day6.hx
@@ -1,9 +1,6 @@
 import haxe.Int64;
 import StringTools.*;
 import Std.*;
-import Math.*;
-import haxe.macro.*;
-import haxe.ds.*;
 import Tools;
 
 using hx.strings.Strings;
@@ -12,31 +9,26 @@ class Day6 {
     static function main() {
         var mp = parsefile();
         solution(mp);
+        
     }
-
-    static function parsefile(): Map<Int,Int64> {
-        var con:AI = [for (i in sys.io.File.getContent('Advent Files_2021/Day6.txt').split(',')) parseInt(i)];
+    static function parsefile(): Map<Int, Int64> {
+        var con = [for (i in sys.io.File.getContent('Advent Files_2021/Day6.txt').split(',')) parseInt(i)];
         var mp: Map<Int, Int64> = [];
         for (i in 0...9) mp.set(i, 0);
         for (i in con) mp.set(i, mp[i] + 1);
         return mp;
     }
 
-    static function solution(mp: Map<Int,Int64>) {
-        var sum:Int64 = 0, sum2:Int64 = 0;
+    static function solution(mp: Map<Int, Int64>) {
+        var sum: Int64 = 0, sum2:Int64 = 0;
         for (j in 1...257) {
-            var six:Int64 = 0, eight:Int64 = 0;
-            for (i in 0...9) {
-                if (i == 0) {
-                    six = mp[0];
-                    eight = mp[0];
-                }
-                else mp[i - 1] = mp[i];
-            }
-            mp[6] += six;
-            mp[8] = eight;
+            var tmp: Int64 = mp[0]; 
+            for (i in 1...9) mp[i - 1] = mp[i];
+
+            mp[6] += tmp;
+            mp[8] = tmp;
             if (j == 80) for (i in mp) sum += i;
-            if (j == 256) for (i in mp) sum2 += i;          
+            if (j == 256) for (i in mp) sum2 += i;       
         }
         Sys.println('Part 1: $sum\nPart 2: $sum2');
     }
