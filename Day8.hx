ad9df73557eb48e3a683d07bd66dba5cdf3e98b5
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-11-15 14:39:19 -0700

    Update Day8.hx

--- Day8.hx
+++ Day8.hx
@@ -1,8 +1,3 @@
-import StringTools.*;
-import Std.*;
-import Math.*;
-import haxe.macro.*;
-import haxe.ds.*;
 import Tools;
 
 using hx.strings.Strings;
@@ -10,71 +5,55 @@ using Lambda;
 
 class Day8 {
     static function main() {
-        var con = parsefile();
-        var part1 = solution1(con);
-        var part2 = solution2(con);
-        Sys.println('Part 1: $part1\nPart 2: $part2');
+        var arr = parsefile();
+        var sol = solution(arr);
+        Sys.println('Part 1: ${sol.ttl}\nPart 2: ${sol.ttl2}');
     }
 
     static inline function parsefile() {
-        var con:AS = [for (i in sys.io.File.getContent('Advent Files_2021/Day8.txt').split('\n')) trim(i)];
-        return con;
+        var arr = [for (i in sys.io.File.getContent('Advent Files_2021/Day8.txt').split('\n')) i.trim().split("|")];
+        return [for (i in arr) [for (j in i) j.trim().split(" ")]];
     }
 
-    static function solution1(con: AS): Int {
-        var ttl = 0;
-        var con = [for (i in con) trim(i.split('|')[1])];
-        for (i in con) {
-            for (j in i.split(' ')) {
-                if (j.length == 2 || j.length == 3 || j.length == 4 || j.length == 7) ttl++;
-            }
-        }
-        return ttl;
-    }
-
-    static function solution2(lst: AS): Int {
-        var con = [for (i in lst) trim(i.split('|')[0])];
-        var con1 = [for (i in lst) trim(i.split('|')[1])];
-        var cnt = 0;
-        var final_arr: AI = [];
-        
-        for (i in con) {
-            var str = "";
-            var mp = new MIS(), mp2 = new MSI();
-            var arr: AS = [], tmp: AS = [];
-
-            for (j in i.split(' ')) {
+    static function solution(arr: AAAS) {
+        var ttl = 0, ttl2 = 0;
+        for (i in arr) {
+            var mp: MIS = [];
+            var fives_sixes: AS = [];
+            for (j in i[0]) {
                 j = alphabetSort(j);
-                if (j.length == 2) mp[1] = j;
-                else if (j.length == 3) mp[7] = j;
-                else if (j.length == 4) mp[4] = j;
-                else if (j.length == 7) mp[8] = j;
-                else arr.push(j);
+                switch(j.length) {
+                    case 2: mp[1] = j;
+                    case 3: mp[7] = j;
+                    case 4: mp[4] = j;
+                    case 7: mp[8] = j;
+                    default: fives_sixes.push(j); 
+                 }
             }
 
-            for (j in arr) {
-                if (j.length == 6) {
-                    if (j.containsAll(mp[1].split('')) == false) mp[6] = j;
-                    else if (j.containsAll(mp[4].split('')) == true) mp[9] = j;
-                    else mp[0] = j; 
-                }
-                else if (j.length == 5) {
-                    if (j.containsAll(mp[1].split('')) == true) mp[3] = j;
-                    else tmp.push(j);
+            fives_sixes.sort((a, b) -> b.length - a.length);
+            for (j in fives_sixes) {
+                if (j.length == 5) {
+                        if (j.containsAll(mp[1].split(''))) mp[3] = j;
+                        else if (mp[6].containsAll(j.split(''))) mp[5] = j;
+                        else mp[2] = j;
+                } else {
+                        if (!j.containsAll(mp[1].split(''))) mp[6] = j;
+                        else if (j.containsAll(mp[4].split(''))) mp[9] = j;
+                        else mp[0] = j;
                 }
             }
-            for (j in tmp) {
-                if (mp[6].containsAll(j.split('')) == true) mp[5] = j;
-                else mp[2] = j;
-            }
-            for (j in mp.keyValueIterator()) mp2[j.value] = j.key;
 
-            for (j in con1[cnt].split(' ')) {
-                str += string(mp2[alphabetSort(j)]);
+            var mp2: MSI = [for (j in mp.keyValueIterator()) j.value => j.key]; 
+            var tmp_ttl = "";
+            for (j in i[1]) {
+                if (j.length == 2 || j.length == 3 || j.length == 4 || j.length == 7) ttl += 1;
+                tmp_ttl += Std.string(mp2[alphabetSort(j)]);
             }
-            final_arr.push(parseInt(str));
-            cnt++;
+
+            ttl2 += Std.parseInt(tmp_ttl);
         }
-        return intSum(final_arr);
+        
+        return {ttl: ttl, ttl2: ttl2};
     }
 }
